7927062ddf6b59f395194224044f15dd57bc0fb5
jess smith <jessica.k.smith@bryanuniversity.edu>
2022-11-29 11:00:21 -0700

    changes

--- week4/graphing/graph.py
+++ week4/graphing/graph.py
@@ -86,43 +86,13 @@ def dijsktra(graph, initial, end):
 
 ##################
 
-def totalCost(graph, initial, end):
-    shortest_paths = {initial: (None, 0)}
-    current_node = initial
-    visited = set()
-    
-    while current_node != end:
-        visited.add(current_node)
-        destinations = graph[current_node]
-        weight_to_current_node = shortest_paths[current_node][1]
-
-        for next_node in destinations:
-            weight = graph[current_node][next_node] + weight_to_current_node
-            if next_node not in shortest_paths:
-                shortest_paths[next_node] = (current_node, weight)
-            else:
-                current_shortest_weight = shortest_paths[next_node][1]
-                if current_shortest_weight > weight:
-                    shortest_paths[next_node] = (current_node, weight)
-
-        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
-        if not next_destinations:
-            return "Route Not Possible"
-        # next node is the destination with the lowest weight
-        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
-
-    # Work back through destinations in shortest path
-    path = []
-    while current_node is not None:
-        path.append(current_node)
-        next_node = shortest_paths[current_node][0]
-        current_node = next_node
-    
-    # Reverse path
-    path = path[::-1]
-    return current_shortest_weight
-    pass
+def totalCost(path):
+    cost = 0
+    for i in range(len(path)-1):
+        cost += graph[path[i]][path[i+1]]
+    return cost
+cost = dijsktra(graph, initial='A', end='H')
 
 print(breadth_first_search(graph, 'A', 'H'))
 print(dijsktra(graph, initial='A', end='H'))
-print(totalCost(graph, initial='A', end='H'))
+print(totalCost(cost))

78ba52e598df7886ae4b889b61e7d12c42cf0196
jess smith <jessica.k.smith@bryanuniversity.edu>
2022-12-08 14:46:16 -0700

    quick sort

--- week6/quickSort/quick_sort.py
+++ week6/quickSort/quick_sort.py
@@ -64,8 +64,116 @@ quick_sort(myList,0,len(myList)-1)
 end_time = time.time()
 #print()
 #print("Sorted Listed: ")
-#print(myList)   
+# print(myList)   
 
 print(f"The execution time is: {end_time-start_time}")
 
 
+###########
+# sort by last element 
+
+
+def quickSort2(sequence):
+    length = len(sequence)
+    if length <=1:
+        return sequence
+    else:
+        pivot = sequence.pop()
+        # print(pivot, "line 81")
+    
+    items_greater = []
+    items_lower = []
+
+    for item in sequence:
+        if item > pivot:
+            items_greater.append(item)
+        else:
+            items_lower.append(item)
+
+    return quickSort2(items_lower) + [pivot] + quickSort2(items_greater)
+
+
+start_time = time.time()
+sequence = [x for x in range(1000)]
+random.shuffle(sequence)
+# [5,2,6,7,15,635,221,634,45,6,37,9,8]
+quickSort2(sequence)
+end_time = time.time()
+print(f"The execution time is: {end_time-start_time}")
+
+
+###########
+# sort by middle element
+
+def qsort(a, low, hi):
+    if(low >= hi):
+        return
+    p = a[(low + hi) // 2]       
+    i = low - 1
+    j = hi + 1
+    while(1):
+        while(1):              
+            i += 1
+            if(a[i] >= p):
+                break
+        while(1):               
+            j -= 1
+            if(a[j] <= p):
+                break
+        if(i >= j):
+            break
+        a[i],a[j] = a[j],a[i]
+    qsort(a, low, j)
+    qsort(a, j+1, hi)
+    return(qsort)
+    
+
+
+start_time = time.time()
+a = [x for x in range(1000)]
+random.shuffle(a)
+# [11,5,6,35,245,86,91,307,74,4,3]
+qsort(a, 0, len(a)-1)
+end_time = time.time()
+# print(a)
+print(f"The execution time is: {end_time-start_time}")
+
+
+#########
+# sort by random element
+# With several run throughs the random selection for a pivot point seems to give the 
+# best lowest timeframe upon return. selecting the first index and the last have switched between
+# being lower than the other per run, while using the middle index has the highest time return of 
+# the four per run.
+
+def randQuickSort(list):
+    def sorting(first, l, r):
+        if r <= l:
+            return
+        pivot_index = random.randint(l,r)
+
+        first[l], first[pivot_index]= first[pivot_index], first[l]
+
+        i = l
+        for j in range(l+1, r+1):
+            if first[j]< first[l]:
+                i +=1
+                first[i], first[j] = first[j], first[i]
+
+        first[i], first[l] = first[l], first[i]
+
+        sorting(first, l, i-i)
+        sorting(first, i+1, r)
+    if list is None or len(list) < 2 :
+        return
+    sorting(list, 0, len(list)-1)
+
+
+start_time = time.time()
+list = [x for x in range(1000)]
+random.shuffle(list)
+# [11,5,6,35,245,86,754,0,68]
+randQuickSort(list)
+end_time = time.time()
+# print(list)
+print(f"The execution time is: {end_time-start_time}")
